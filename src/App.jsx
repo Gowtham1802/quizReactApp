7a81178b4e7e84f1b1d65b1eed07329c61b13f7e
Gowtham1802 <gowthama7575@gmail.com>
2023-11-29 13:01:00 +0530

    add update codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import Question from "./components/quizapp/questio";
 import Answer from "./components/quizapp/answer";
+import Login from "./components/loginpage/login";
 import "./App.css";
 
 const App = () => {
   return (
-    <div id="page1" className="w-[100vw] min-h-screen flex flex-col justify-center items-center  ">
-      <div
-        id="box"
-        className="w-[40rem] rounded-l-sm	 h-[25rem] flex flex-col justify-around items-center shadow-md"
-      >
-        <Question />
-        <Answer />
-        {/* <LoginPage></LoginPage> */}
-      </div>
+    // <div
+    //   id="page1"
+    //   className="w-[100vw] min-h-screen flex flex-col justify-center items-center  "
+    // >
+    //   <div
+    //     id="box"
+    //     className="w-[40rem] rounded-l-sm	 h-[25rem] flex flex-col justify-around items-center shadow-md"
+    //   >
+    //     {/* <Question /> */}
+    //     {/* <Answer /> */}
+    //
+    //   </div>
+    // </div>
+    <div>
+      <Login />
     </div>
   );
 };
--- src/components/loginpage/login.jsx
+++ src/components/loginpage/login.jsx
@@ -1,25 +1,37 @@
-import "../components/login.css";
+import "../loginpage/login.css";
 
-const LoginPage = () => {
+const Login = () => {
   return (
     <div>
       <div className="ip-container">
-        <h1 className="text-center; text-[#00dfc4;]">Login</h1>
+        <h1 className="text-center; text-[#00dfc4;] text-teal-400 font-extrabold">
+          LOGIN
+        </h1>
         <div className="inputBox">
           <input type="text" required="required" />
-          <span>Accound id</span>
+          <span>USER ID</span>
         </div>
         <div className="inputBox">
           <input type="password" required="required" />
           <span>PassWord</span>
         </div>
-        <a href="adminn.html">
-          <button>LOGIN</button>
+        <a href="question.jsx">
+          <button className="text-teal-400 font-medium">Login</button>
         </a>
-        <a href="">Forget password?</a>
-        <p className="text-[#ccc;]"></p> 
+        <a href="">
+          <button
+            className="text-teal-400 font-medium"
+            // onClick={signInWithGoogle}
+          >
+            Google Sign
+          </button>
+        </a>
+        <a href="" className="text-teal-400 font-medium">
+          Forget password?
+        </a>
+        <p className="text-[#ccc;]"></p>
       </div>
     </div>
   );
 };
-export default LoginPage;
+export default Login;
