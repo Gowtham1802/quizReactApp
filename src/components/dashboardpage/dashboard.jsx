90d47eb3079265306169ee7aa1c4c1e6640c4dad
Gowtham1802 <gowthama7575@gmail.com>
2023-12-21 15:42:22 +0530

    add

--- src/components/dashboardpage/dashboard.jsx
+++ src/components/dashboardpage/dashboard.jsx
@@ -31,95 +31,245 @@ const DashBoard = () => {
 
       <div className="category-list">
         <div className="flex-box ">
-          <div className="flex-item flex-item-1 text-[20px] font-bold italic hover:not-italic ">
+          <div
+            className="flex-item flex-item-1 text-[20px] font-bold italic hover:not-italic "
+            id="music"
+          >
             Musical Theaters
             <div className="category-img">
               <GiTheater className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
-            <div></div>
           </div>
-          <div className="flex-item flex-item-2 text-[20px] font-bold italic hover:not-italic">
+
+          <div
+            className="flex-item flex-item-2 text-[20px] font-bold italic hover:not-italic"
+            id="book"
+          >
             Books
             <div className="category-img">
-              <SiBookstack className="text-[2rem]" />{" "}
+              <SiBookstack className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-3 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-3 text-[20px] font-bold italic hover:not-italic"
+            id="flim"
+          >
             Flims
             <div className="category-img">
               <BiCameraMovie className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-4 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-4 text-[20px] font-bold italic hover:not-italic"
+            id="mic"
+          >
             Music
             <div className="category-img">
               <BsMusicNoteList className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-5 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-5 text-[20px] font-bold italic hover:not-italic"
+            id="tv"
+          >
             Television
             <div className="category-img">
               <PiTelevisionBold className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-6 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-6 text-[20px] font-bold italic hover:not-italic"
+            id="vg"
+          >
             Video Games
             <div className="category-img">
               <GrGamepad className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-7 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-7 text-[20px] font-bold italic hover:not-italic"
+            id="comp"
+          >
             Computers
             <div className="category-img">
               <FaComputer className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-8 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-8 text-[20px] font-bold italic hover:not-italic"
+            id="maths"
+          >
             Mathematics
             <div className="category-img">
               <TbMathSymbols className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-9 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-9 text-[20px] font-bold italic hover:not-italic"
+            id="science"
+          >
             Science
             <div className="category-img">
               <MdScience className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-10 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-10 text-[20px] font-bold italic hover:not-italic"
+            id="sports"
+          >
             Sports
             <div className="category-img">
               <MdOutlineSportsSoccer className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-11 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-11 text-[20px] font-bold italic hover:not-italic"
+            id="history"
+          >
             History
             <div className="category-img">
               <MdOutlineHistoryEdu className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-12 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-12 text-[20px] font-bold italic hover:not-italic"
+            id="politics"
+          >
             Politics
             <div className="category-img">
               <FaLandmarkFlag className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-13 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-13 text-[20px] font-bold italic hover:not-italic"
+            id="vehicle"
+          >
             Vehicles
             <div className="category-img">
               <TbCarSuv className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-14 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-14 text-[20px] font-bold italic hover:not-italic"
+            id="cartoon"
+          >
             Cartoon
             <div className="category-img">
               <MdOutlineSmartToy className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex-item flex-item-15 text-[20px] font-bold italic hover:not-italic">
+          <div
+            className="flex-item flex-item-15 text-[20px] font-bold italic hover:not-italic"
+            id="gadget"
+          >
             Gadgets
             <div className="category-img">
               <SiEngadget className="text-[2rem]" />
+              <div id="btn1">
+                <input
+                  type="button"
+                  onclick="alert('Hello World!')"
+                  value="Click Me!"
+                />
+              </div>
             </div>
           </div>
         </div>
